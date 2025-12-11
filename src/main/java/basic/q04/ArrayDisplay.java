45ac81a53f3575912f76a6cd9ea8046d458c20fc
Masaki Takao <yakyubou.1222@icloud.com>
2025-12-11 13:49:18 +0900

    q04のコミット

---
+++ src/main/java/basic/q04/ArrayDisplay.java
@@ -0,0 +1,19 @@
+package basic.q04;
+
+public class ArrayDisplay {
+
+	public static void main(String[] args) {
+		String[] fruit = new String[5];
+		fruit[0] = "りんご";
+		fruit[1] = "ばなな";
+		fruit[2] = "みかん";
+		fruit[3] = "ぶどう";
+		fruit[4] = "すいか";
+		//String[] fruit = {"りんご","ばなな","みかん","ぶどう","すいか"};//別解
+		
+		for (int i = 0; i < fruit.length; i++) {
+			System.out.println(fruit[i]);
+		}
+	}
+
+}
