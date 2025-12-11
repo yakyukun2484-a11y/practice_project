96d1dc86769fad672ba14e9bea6e340b18c75592
Masaki Takao <yakyubou.1222@icloud.com>
2025-12-11 14:47:26 +0900

    q07のコミット

---
+++ src/main/java/basic/q07/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package basic.q07;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				if (i * j < 10) {
+					System.out.print("  ");
+				}else {
+					System.out.print(" ");
+				}
+				System.out.print(i * j);
+			}
+			System.out.println();
+		}
+	}
+}
