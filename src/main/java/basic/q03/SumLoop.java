73d016959ac1a04f604a6b25d531a0a519293fd4
Masaki Takao <yakyubou.1222@icloud.com>
2025-12-11 13:01:00 +0900

    q03のコミット

---
+++ src/main/java/basic/q03/SumLoop.java
@@ -0,0 +1,15 @@
+package basic.q03;
+
+public class SumLoop {
+
+	public static void main(String[] args) {
+		int x = 100;
+		int sum = 0;
+		for (int i = 1; i <= x; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
