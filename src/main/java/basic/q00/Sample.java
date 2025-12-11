d9eb78c6c94d186ffec6b01ded1a32d64b484457
Masaki Takao <yakyubou.1222@icloud.com>
2025-12-11 12:31:23 +0900

    q01をコミット

---
+++ src/main/java/basic/q00/Sample.java
@@ -0,0 +1,10 @@
+package basic.q00;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("sampleです。");
+
+	}
+
+}
--- src/main/java/basic/q01/SubtractLoop.java
+++ src/main/java/basic/q01/SubtractLoop.java
@@ -3,7 +3,10 @@ package basic.q01;
 public class SubtractLoop {
 
 	public static void main(String[] args) {
-		System.out.println("sampleです。");
+		int x = 100;
+		while (x >= 0) {
+			System.out.println(x--);
+		}
 
 	}
 
