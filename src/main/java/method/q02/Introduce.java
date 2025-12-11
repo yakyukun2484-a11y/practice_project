dd43a4a5b6ea530091b619b7920fed366c19ffc8
Masaki Takao <yakyubou.1222@icloud.com>
2025-12-11 17:02:43 +0900

    q02のコミット

---
+++ src/main/java/method/q02/Introduce.java
@@ -0,0 +1,14 @@
+package method.q02;
+
+public class Introduce {
+
+	public static void main(String[] args) {
+		System.out.println("私の名前は" + selfIntroduce("") + "です。");
+	}
+
+	public static String selfIntroduce(String n) {
+		String name = "山田";
+		return name;
+	}
+
+}
