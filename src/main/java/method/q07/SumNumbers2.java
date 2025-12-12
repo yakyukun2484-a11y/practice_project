662fdb7ff6c92648bcb5888bd79b5e515b5e3d00
Masaki Takao <yakyubou.1222@icloud.com>
2025-12-12 14:06:41 +0900

    q07のコミット

---
+++ src/main/java/method/q07/SumNumbers2.java
@@ -0,0 +1,18 @@
+package method.q07;
+
+public class SumNumbers2 {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		double num2 = 3.3;
+		System.out.println("第一引数（整数）：" + num1);
+		System.out.println("第二引数（実数）：" + num2);
+		System.out.println("加算結果：" + calculateSum(num1, num2));
+	}
+
+	public static double calculateSum(int a, double b) {
+		double sum = a + b;
+		return sum;
+	}
+
+}
