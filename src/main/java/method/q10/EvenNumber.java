51ff80271cd59b8c5e2b514aa8455a1d41bfcb2c
Masaki Takao <yakyubou.1222@icloud.com>
2025-12-13 16:50:42 +0900

    q10のコミット

--- src/main/java/method/q10/EvenNumber.java
+++ src/main/java/method/q10/EvenNumber.java
@@ -1,10 +1,24 @@
 package method.q10;
 
+import java.util.Arrays;
+
 public class EvenNumber {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		//int [] a = new int[10];
+		int[] a = { 3, 2, 5, 6, 7, 25, 10, 51, 88, 98 };
+		System.out.println(Arrays.toString(a) + "には、偶数が" + getEvenNumbers(a) + "個あります。");
 
+	}
+
+	public static int getEvenNumbers(int[] numbers) {
+		int count = 0;
+		for (int num : numbers) {
+			if (num % 2 == 0) {
+				count++;
+			}
+		}
+		return count;
 	}
 
 }
