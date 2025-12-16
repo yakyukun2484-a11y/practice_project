ef26a2ee476101c22c75c4e7f7d7905ecf579a96
Masaki Takao <yakyubou.1222@icloud.com>
2025-12-16 17:09:04 +0900

    practice01のコミット

---
+++ src/main/java/practice/practice01/Employee.java
@@ -0,0 +1,13 @@
+package practice.practice01;
+
+public class Employee {
+
+	public String name;
+	int age;
+	
+	public void showInfo() {
+		System.out.println("名前：" +  name);
+		System.out.println("年齢：" + age );
+	}
+
+}
